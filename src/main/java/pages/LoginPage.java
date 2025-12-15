94230bb8e3801411153b00d2c04320960b262335
Shchatkovskaya Elena <shchatkovskaya@hostco.ru>
2025-12-15 21:52:53 +0300

    add logging

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -3,6 +3,8 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import utils.Log;
+
 public class LoginPage {
 	private WebDriver driver;
 
@@ -25,6 +27,7 @@ public class LoginPage {
 	}
 	
 	public void clickLogin() {
+		Log.info("Clicking login button...");
 		driver.findElement(loginButton).click();
 	}
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,19 +5,23 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.Log;
 
 public class LoginTest extends BaseTest {
 	
 	@Test
 	public void testValidLogin() {
 		
+		Log.info("Starting login test...");
 		LoginPage loginPage = new LoginPage(driver);
 		
+		Log.info("Adding credentials...");
 		loginPage.enterUserName("admin@yourstore.com");
 		loginPage.enterPassword("admin");
 		loginPage.clickLogin();
 		
 		System.out.println("Заголовок этой страницы: "+driver.getTitle());
+		Log.info("Verifying page title...");
 		Assert.assertEquals(driver.getTitle(), "Один момент…");
 		
 		
