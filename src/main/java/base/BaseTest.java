2804ef0978a7a9824db2ff53ae47f4c03834dd5d
Shchatkovskaya Elena <shchatkovskaya@hostco.ru>
2025-12-17 20:39:04 +0300

    add reports and screenshots

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,14 +2,35 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.observer.entity.MediaEntity;
+
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class BaseTest {
 
 	protected WebDriver driver;
+	protected ExtentReports extent;
+	protected ExtentTest test;
+
+	@BeforeSuite
+	public void setupReport() {
+		extent = ExtentReportManager.getReportInstance();
+	}
+
+	@AfterSuite
+	public void tearDownReport() {
+		extent.flush();
+	}
 
 	@BeforeMethod
 	public void setUp() {
@@ -22,7 +43,16 @@ public class BaseTest {
 	}
 
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) {
+		
+		if(result.getStatus() == ITestResult.FAILURE) {
+			
+			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "LoginFailure");
+			test.fail("Test failed... Check screenshot", 
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+		}
+		
+		
 		if (driver != null) {
 			Log.info("Closing Browser...");
 //			driver.quit();
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,27 +5,62 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest {
-	
+
 	@Test
-	public void testValidLogin() {
-		
+	public void testLoginWithInvalidCredentials () throws InterruptedException {
+
 		Log.info("Starting login test...");
+		test = ExtentReportManager.createTest("Login Test  with Invalid Credentials");
+		test.info("Navigating to URL");
+
 		LoginPage loginPage = new LoginPage(driver);
-		
+
 		Log.info("Adding credentials...");
+		test.info("Adding Credentials");
 		loginPage.enterUserName("admin@yourstore.com");
-		loginPage.enterPassword("admin");
+		loginPage.enterPassword("admin123");
+		test.info("Clicking om Login button");
 		loginPage.clickLogin();
 		
-		System.out.println("Заголовок этой страницы: "+driver.getTitle());
-		Log.info("Verifying page title...");
-		Assert.assertEquals(driver.getTitle(), "Один момент…");
-		
-		
-	}
+		Thread.sleep(5000);
 	
+
+		System.out.println("Заголовок этой страницы: " + driver.getTitle());
+		Log.info("Verifying page title...");
+		test.info("Verifing page title");
+		Assert.assertEquals(driver.getTitle(), "Один момент…");
+
+		test.pass("Login Successful");
+
+	}
+
+	@Test
+	public void testValidLogin() {
+
+		Log.info("Starting login test...");
+		test = ExtentReportManager.createTest("Login Test");
+		test.info("Navigating to URL");
+
+		LoginPage loginPage = new LoginPage(driver);
+
+		Log.info("Adding credentials...");
+		test.info("Adding Credentials");
+		loginPage.enterUserName("admin@yourstore.com");
+		loginPage.enterPassword("admin");
+		test.info("Clicking om Login button");
+		loginPage.clickLogin();
+
+		System.out.println("Заголовок этой страницы: " + driver.getTitle());
+		Log.info("Verifying page title...");
+		test.info("Verifing page title");
+		Assert.assertEquals(driver.getTitle(), "Один момент…");
+
+		test.pass("Login Successful");
+
+	}
 
 }
